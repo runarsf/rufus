4dee5b04df2f7de6a04ddc3c8e35884fb88183d5
Runar <runar.fredagsvik@gmail.com>
2017-12-05 22:44:28 +0100

    added some shit

--- rufus.py
+++ rufus.py
@@ -1,37 +1,54 @@
-import discord
-import asyncio
+#import asyncio
 import random
 import pickle
 import os
+import discord
 from data import token
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('-------')
-	
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-------')
+    await client.change_presence(game=discord.Game(name='>help'))
+
 @client.event
 async def on_message(message):
-	if message.content.startswith('>ping'):
-		await client.send_message(message.channel, 'pong')
-	elif message.content.startswith('>flip'):
-		flip = random.choice(['Heads', 'Tails'])
-		await client.send_message(message.channel, flip)
-	elif message.content.startswith('>addquote'):
-		if not os.path.isfile('quotes.pk1'):
-			quote_list = []
-		else:
-			with open('quotes.pk1', 'rb') as quotes:
-				quote_list = pickle.load(quotes)
-		quote_list.append(message.content[9:])
-		with open('quotes.pk1', 'wb') as quotes:
-			pickle.dump(quote_list, quotes)
-	elif message.content.startswith('>quote'):
-		with open('quotes.pk1', 'rb') as quotes:
-			quote_list = pickle.load(quotes)
-		await client.send_message(message.channel, random.choice(quote_list))
+	# Ping
+    if message.content.startswith('>ping'):
+        await client.send_message(message.channel, 'pong')
+
+	# Rolls a dice
+    if message.content.startswith('>roll'):
+        roll = random.choice(['1', '2', '3', '4', '5', '6'])
+        await client.send_message(message.channel, roll)
+
+	# Add quote to quotes.pk1
+    if message.content.startswith('>addquote'):
+        if not os.path.isfile('quotes.pk1'):
+            quote_list = []
+        else:
+            with open('quotes.pk1', 'rb') as quotes:
+                quote_list = pickle.load(quotes)
+        quote_list.append(message.content[9:])
+        with open('quotes.pk1', 'wb') as quotes:
+            pickle.dump(quote_list, quotes)
+
+	# Get random quote from quotes.pk1
+    if message.content.startswith('>quote'):
+        with open('quotes.pk1', 'rb') as quotes:
+            quote_list = pickle.load(quotes)
+        await client.send_message(message.channel, random.choice(quote_list))
+
+	# Change game
+    if message.content.startswith('>setgame'):
+        await client.send_message(message.channel, 'hello')
+        msg = await client.wait_for_message(author=message.author, content='hello')
+        await client.change_presence(game=discord.Game(name='Set'))
+        await client.send_message(message.channel, 'ye')
+
+
 client.run(token)
