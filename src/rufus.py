6cac4d7c0319db34e4a92a6f0ee8c9b67a23a1b3
Runar <runarsf@protonmail.com>
2018-09-10 20:27:56 +0200

    added config check

--- src/rufus.py
+++ src/rufus.py
@@ -1,31 +1,10 @@
 """ Rufus """
 import os
-
-# check config
-if not os.path.isfile('config.py'):
-    confile = open('config.py', 'w+')
-    confile.write('test')
-    confile.close()
-
 import atexit
 import discord
 from discord.ext import commands
 import config as c
 # pip install -r .\requirements.txt
-
-# token = ''
-# prefix = '>'
-# game = 'in his container'
-# games = ['1','2']
-# description = 'Descropt'
-# start_msg = ''
-
-# announcements_id = '290272570425737216'
-# owner_id = '177098893069254656'
-# dev_id = ['85467784179351552', '198414561706115072']
-# admin_id = ['315908137976856579', '198414561706115072']
-# swears = ['frick', 'heck']
-# mention = ['rufus', 'runar']
 
 STARTUP_EXTENSIONS = ['cogs.commands',
                       'cogs.admin',
