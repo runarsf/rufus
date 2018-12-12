6f682e6aba30a08ba430422ed548be9170d32b52
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-12-12 15:36:17 +0100

    mhm

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -149,20 +149,9 @@ class Commands:
         """
         mend = ctx.message.server.get_member_named(userMentioned)
         try:
-            #mend = ctx.message.server.get_member_named(userMentioned)
-            #await self.bot.say(ctx.message.Object.created_at(userMentioned))
-            #await self.bot.say(str(userMentioned))
-            embed=discord.Embed(title=title, url=httpurl, description=descropt, color=0x0080c0)
-            embed.set_author(name=author_name, url=author_link,icon_url=author_icon)
-            embed.set_thumbnail(url=icon)
-            embed.add_field(name=field1_name, value=field1_value, inline=False)
-            embed.add_field(name=field2_name, value=field_2value, inline=True)
-            embed.set_footer(text=footer_text)
-
-            await self.bot.say(embed=embed)
-            embed = discord.Embed(title="Tile", description="Desc", color=0x3b4d7c)
-                    embed.add_field(name="Fiel1", value="hi", inline=False)
-                    embed.add_field(name="Field2", value="hi2", inline=False)
+            mend = ctx.message.server.get_member_named(userMentioned)
+            await self.bot.say(ctx.message.Object.created_at(userMentioned))
+            await self.bot.say(str(userMentioned))
         except:
             await self.bot.say('It would help if, you know, the mention was.. VALID...')
 
