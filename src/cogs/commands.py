e770be0db5d46acf53011471a7b12588ded20a8a
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-12-12 15:21:07 +0100

    info test embed

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -143,17 +143,28 @@ class Commands:
         await self.bot.send_message(ctx.message.server.get_member_named(userToInvite), invite)
         await self.bot.say('Invite link sent to **{}**.'.format(ctx.message.server.get_member_named(userToInvite)))
 
-        @commands.command(pass_context=True)
-        async def info(self, ctx, *, userMentioned: str):
-            """ Shows information about the specified user.
-            """
-            mend = ctx.message.server.get_member_named(userMentioned)
-            try:
-                mend = ctx.message.server.get_member_named(userMentioned)
-                await self.bot.say(ctx.message.Object.created_at(userMentioned))
-                await self.bot.say(str(userMentioned))
-            except:
-                await self.bot.say('It would help if, you know, the mention was.. VALID...')
+    @commands.command(pass_context=True)
+    async def info(self, ctx, *, userMentioned: str):
+        """ Shows information about the specified user.
+        """
+        mend = ctx.message.server.get_member_named(userMentioned)
+        try:
+            #mend = ctx.message.server.get_member_named(userMentioned)
+            #await self.bot.say(ctx.message.Object.created_at(userMentioned))
+            #await self.bot.say(str(userMentioned))
+            embed=discord.Embed(title=title, url=httpurl, description=descropt, color=0x0080c0)
+            embed.set_author(name=author_name, url=author_link,icon_url=author_icon)
+            embed.set_thumbnail(url=icon)
+            embed.add_field(name=field1_name, value=field1_value, inline=False)
+            embed.add_field(name=field2_name, value=field_2value, inline=True)
+            embed.set_footer(text=footer_text)
+
+            await self.bot.say(embed=embed)
+            embed = discord.Embed(title="Tile", description="Desc", color=0x3b4d7c)
+                    embed.add_field(name="Fiel1", value="hi", inline=False)
+                    embed.add_field(name="Field2", value="hi2", inline=False)
+        except:
+            await self.bot.say('It would help if, you know, the mention was.. VALID...')
 
 
 def setup(bot):
