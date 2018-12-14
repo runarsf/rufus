f95390d798c13c3de9e622622d9ea91aa0c8d9bc
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-12-14 07:53:48 +0100

    info

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -147,13 +147,13 @@ class Commands:
         """
         if not user:
             return
-        userDescription = """id: {}\
-                             nick: {}\
-                             created at: {}\
-                             joined at: {}\
-                             game: {}\
-                             top role: {}\
-                             bot? {}\
+        userDescription = """id: {}\n\
+                             nick: {}\n\
+                             created at: {}\n\
+                             joined at: {}\n\
+                             game: {}\n\
+                             top role: {}\n\
+                             bot? {}\n\
                              avatar: {}""".format(user.id, user.nick, user.created_at, user.joined_at, user.game, user.top_role, user.bot, user.avatar_url)
         embed=discord.Embed(title='{}#{}'.format(user.name, user.discriminator), description=userDescription, color=0x114455)
         embed.set_thumbnail(url=(user.avatar_url))
