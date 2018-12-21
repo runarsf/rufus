acaed1fc2f6fc609e03d762f24161def974e08f3
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-12-21 12:19:06 +0100

    mhm

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -103,67 +103,12 @@ class Commands:
 
     @commands.command()
     async def wolfram(self, *, query: str = ''):
-        client = wolframalpha.Client(c.wolfram_api_key)
-
-        def search_wiki(keyword=''):
-          searchResults = wikipedia.search(keyword)
-          if not searchResults:
-            print("No result from Wikipedia")
-            return
-          try:
-            page = wikipedia.page(searchResults[0])
-          except wikipedia.DisambiguationError, err:
-            page = wikipedia.page(err.options[0])
-
-          wikiTitle = str(page.title.encode('utf-8'))
-          wikiSummary = str(page.summary.encode('utf-8'))
-          print(wikiSummary)
-        def search(text=''):
-          res = client.query(text)
-          if res['@success'] == 'false':
-             print('Question cannot be resolved')
-          else:
-            result = ''
-            # pod[0] is the question
-            pod0 = res['pod'][0]
-            # pod[1] may contains the answer
-            pod1 = res['pod'][1]
-            # checking if pod1 has primary=true or title=result|definition
-            if (('definition' in pod1['@title'].lower()) or ('result' in  pod1['@title'].lower()) or (pod1.get('@primary','false') == 'true')):
-              # extracting result from pod1
-              result = resolveListOrDict(pod1['subpod'])
-              print(result)
-              question = resolveListOrDict(pod0['subpod'])
-              question = removeBrackets(question)
-              primaryImage(question)
-            else:
-              # extracting wolfram question interpretation from pod0
-              question = resolveListOrDict(pod0['subpod'])
-              # removing unnecessary parenthesis
-              question = removeBrackets(question)
-              # searching for response from wikipedia
-              search_wiki(question)
-              primaryImage(question)
-
-        def removeBrackets(variable):
-          return variable.split('(')[0]
-
-        def resolveListOrDict(variable):
-          if isinstance(variable, list):
-            return variable[0]['plaintext']
-          else:
-            return variable['plaintext']
-
-        def primaryImage(title=''):
-            url = 'http://en.wikipedia.org/w/api.php'
-            data = {'action':'query', 'prop':'pageimages','format':'json','piprop':'original','titles':title}
-            try:
-                res = requests.get(url, params=data)
-                key = res.json()['query']['pages'].keys()[0]
-                imageUrl = res.json()['query']['pages'][key]['original']['source']
-                print(imageUrl)
-            except Exception, err:
-        print('Exception while finding image:= '+str(err))
+        app_id = c.wolfram_api_key
+        client = wolframalpha.Client(app_id)
+        my_input = query
+        res = client.query(my_input)
+        answer = next(res.results).text
+        await self.bot.say(answer)
 
     @commands.command(pass_context=True)
     async def poke(self, ctx, user: discord.User = '', *, message: str = ''):
