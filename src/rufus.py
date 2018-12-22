fb3005a9e53febb0fd1e831e6a358568fb79cc13
Runar <runarsf@protonmail.com>
2018-12-22 20:36:29 +0100

    retest

--- src/rufus.py
+++ src/rufus.py
@@ -100,7 +100,7 @@ async def on_message(message):
         if message.author == bot.user:
             return
         if any(word in message.content for word in c.swears):
-            await bot.send_file(message.channel, 'img/christ.jpg')
+            await bot.send_file(message.channel, './img/christ.jpg')
         if any(mention in message.content for mention in c.mention):
             print(message.author.name + ' ' + message.author.mention + ' :: ' + message.server.name + ' :: ' + message.content)
     finally:
