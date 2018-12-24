5cb809d8c6d4a728750fb5a61bf89d09a021be6a
Runar <runarsf@protonmail.com>
2018-12-24 15:43:20 +0100

    asd

--- src/rufus.py
+++ src/rufus.py
@@ -99,6 +99,8 @@ async def on_message(message):
     try:
         if message.author == bot.user:
             return
+        if any(message.content in cuss for cuss in c.swears):
+            await bot.send_file(message.channel, r"img/christ.jpg",filename="christ")
         #if any(word in message.content for word in c.swears):
         #    await bot.send_file(message.channel, 'img/christ.jpg')
         if any(mention in message.content for mention in c.mention):
